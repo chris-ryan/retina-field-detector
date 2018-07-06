4b74522a1a32814d908e0259000c0b3bef02ea55
Chris Ryan <chris.ryan@ctc.usyd.edu.au>
2018-07-06 22:28:22 +1000

    init commit

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+express.static.mime.types['wasm'] = 'application/wasm';
+
+app.use(express.static('public'))
+// app.use('js', express.static('js'));
+// app.use('images', express.static('images'));
+
+//app.get('/', (req, res) => res.send("hello"))
+
+let server = app.listen(3000, () => console.log('html server listening on port 3000'));
